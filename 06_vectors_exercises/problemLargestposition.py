e0614b6ff6d601de961869dcf1112417dc77a67b
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-20 17:33:54 -0600

    vectors-exercises problems

---
+++ 06_vectors_exercises/problemLargestposition.py
@@ -0,0 +1,26 @@
+def ingresar_numero(mensaje, tipo='float'):
+    while True:
+        try:
+            entrada = input(mensaje)
+            if tipo == 'int':
+                return int(entrada)
+            return float(entrada)
+        except ValueError:
+            print("Error: Ingresa un número válido.")
+    
+def main():
+    N = ingresar_numero('¿Cuántos números vas a introducir? ', tipo='int')
+    vector = []
+    par = []
+
+    for _ in range(N):
+        vector.append(ingresar_numero('Introduzca un número: '))
+
+    print('-' * 30)
+    valor_alto = max(vector)
+    index_alto = vector.index(valor_alto)
+    print(f"VALOR MÁS ALTO: {valor_alto}.")
+    print(f"POSICIÓN DEL VALOR MÁS ALTO: {index_alto}")
+
+if __name__ == '__main__':
+    main()
